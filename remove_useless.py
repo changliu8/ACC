e7c820e14ae0ae0c3202c358db4bfce5fc9a4684
Chang Liu <cliu162@uottawa.ca>
2025-06-23 17:14:36 -0400

    merge test into remove_useless

--- remove_useless.py
+++ remove_useless.py
@@ -8,6 +8,8 @@ tmp = df.columns.to_list()
 print(len(df.columns.to_list()))
 
 
+#find all static data and remove
+
 static_data = []
 
 for item in tmp:
@@ -16,6 +18,31 @@ for item in tmp:
 
 df = df.drop(columns=static_data)
 
-print(len(df.columns.to_list()))
 
-df.to_csv('output_clean.csv',index=False)
+#remove graphics car coordinates since it is a vector contains x,y,z and not well formated.
+test = df["Graphics_car_coordinates"]
+df = df.drop(["Graphics_car_coordinates"],axis=1)
+
+xs = []
+ys = []
+zs = []
+
+# split the vec<x,y,z> in graphics_car_coordinates into x,y,z
+
+for item in test:
+    #tmp = test.iloc[0]
+    vec = item[item.find('(')+1:item.find(')')]
+    x = vec[vec.find('x')+2:vec.find(',')]
+    y = vec[vec.find('y')+2:vec.find('z')-2]
+    z = vec[vec.find('z')+2:]
+    xs.append(x)
+    ys.append(y)
+    zs.append(z)
+# add the data into the dataframce
+df["Graphics_car_coordinate_x"] = xs
+df["Graphics_car_coordinate_y"] = ys
+df["Graphics_car_coordinate_z"] = zs
+
+df.to_csv('output_clean_split.csv',index=False)
+
+
--- test.py
+++ test.py
@@ -2,13 +2,35 @@ import numpy as np
 import pandas as pd
 import os
 
-df = pd.read_csv(os.path.join(os.getcwd(),r'output.csv'))
+
+df = pd.read_csv(os.path.join(os.getcwd(),r'output_clean.csv'))
 
 test = df["Graphics_car_coordinates"]
+print(len(df.columns.to_list()))
+df = df.drop(["Graphics_car_coordinates"],axis=1)
+print(len(df.columns.to_list()))
 
-tmp = test.iloc[0]
-vec = tmp[tmp.find('(')+1:tmp.find(')')]
-x = vec[vec.find('x')+2:vec.find(',')]
-y = vec[vec.find('y')+2:vec.find('z')-2]
-z = vec[vec.find('z')+2:]
-print(x,y,z)
+
+xs = []
+ys = []
+zs = []
+
+for item in test:
+    #tmp = test.iloc[0]
+    vec = item[item.find('(')+1:item.find(')')]
+    x = vec[vec.find('x')+2:vec.find(',')]
+    y = vec[vec.find('y')+2:vec.find('z')-2]
+    z = vec[vec.find('z')+2:]
+    xs.append(x)
+    ys.append(y)
+    zs.append(z)
+
+df["Graphics_car_coordinate_x"] = xs
+df["Graphics_car_coordinate_y"] = ys
+df["Graphics_car_coordinate_z"] = zs
+
+print(len(df.columns.to_list()))
+
+print(len(xs),len(ys),len(zs))
+
+df.to_csv('output_clean_split.csv',index=False)
