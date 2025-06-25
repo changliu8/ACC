54cb8c353dffed74aeff805882af656c1f55cf38
Chang Liu <cliu162@uottawa.ca>
2025-06-24 23:58:34 -0400

    only keep essential features

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 import numpy as np
 import pandas as pd
 
+df = pd.read_csv("output_clean_split.csv")
+
+
+Y = df["Physics_speed_kmh"]
+X = df.drop(["Physics_speed_kmh"],axis=1)
+
+
+
+
--- remove_useless.py
+++ remove_useless.py
@@ -1,8 +1,9 @@
 import numpy as np
 import pandas as pd
 import os
+import matplotlib.pyplot as plt
 
-df = pd.read_csv(os.path.join(os.getcwd(),r'output.csv'))
+df = pd.read_csv(os.path.join(os.getcwd(),r'acc_maps.csv'))
 
 tmp = df.columns.to_list()
 print(len(df.columns.to_list()))
@@ -43,6 +44,82 @@ df["Graphics_car_coordinate_x"] = xs
 df["Graphics_car_coordinate_y"] = ys
 df["Graphics_car_coordinate_z"] = zs
 
-df.to_csv('output_clean_split.csv',index=False)
 
+# remove invalid lap
+
+df = df[df['Graphics_is_valid_lap'] == True]
+
+feature_list = ["Physics_wheel_angular_s_front_left",
+                "Physics_wheel_angular_s_front_right",
+                "Physics_wheel_angular_s_rear_left",
+                "Physics_wheel_angular_s_rear_right",
+                "Physics_slip_ratio_front_left",
+                "Physics_slip_ratio_front_right",
+                "Physics_slip_ratio_rear_left",
+                "Physics_slip_ratio_rear_right",
+                "Physics_slip_angle_front_left",
+                "Physics_slip_angle_front_right",
+                "Physics_slip_angle_rear_left",
+                "Physics_slip_angle_rear_right",
+                "Physics_wheel_slip_front_left",
+                "Physics_wheel_slip_front_right",
+                "Physics_wheel_slip_rear_left",
+                "Physics_wheel_slip_rear_right",
+                "Physics_brake",
+                "Physics_gas",
+                "Physics_brake_pressure_front_left",
+                "Physics_brake_pressure_front_right",
+                "Physics_brake_pressure_rear_left",
+                "Physics_brake_pressure_rear_right",
+                "Physics_brake_temp_front_left",
+                "Physics_brake_temp_front_right",
+                "Physics_brake_temp_rear_left",
+                "Physics_brake_temp_rear_right",
+                "Physics_tyre_contact_normal_front_left_x",
+                "Physics_tyre_contact_normal_front_left_y",
+                "Physics_tyre_contact_normal_front_left_z",
+                "Physics_tyre_contact_normal_front_right_x",
+                "Physics_tyre_contact_normal_front_right_y",
+                "Physics_tyre_contact_normal_front_right_z",
+                "Physics_tyre_contact_normal_rear_left_x",
+                "Physics_tyre_contact_normal_rear_left_y",
+                "Physics_tyre_contact_normal_rear_left_z",
+                "Physics_tyre_contact_normal_rear_right_x",
+                "Physics_tyre_contact_normal_rear_right_y",
+                "Physics_tyre_contact_normal_rear_right_z",
+                "Physics_tyre_contact_point_front_left_x",
+                "Physics_tyre_contact_point_front_left_y",
+                "Physics_tyre_contact_point_front_left_z",
+                "Physics_tyre_contact_point_front_right_x",
+                "Physics_tyre_contact_point_front_right_y",
+                "Physics_tyre_contact_point_front_right_z",
+                "Physics_tyre_contact_point_rear_left_x",
+                "Physics_tyre_contact_point_rear_left_y",
+                "Physics_tyre_contact_point_rear_left_z",
+                "Physics_tyre_contact_point_rear_right_x",
+                "Physics_tyre_contact_point_rear_right_y",
+                "Physics_tyre_contact_point_rear_right_z",
+                "Physics_tyre_core_temp_front_left",
+                "Physics_tyre_core_temp_front_right",
+                "Physics_tyre_core_temp_rear_left",
+                "Physics_tyre_core_temp_rear_right",
+                "Graphics_mfd_tyre_pressure_front_left",
+                "Graphics_mfd_tyre_pressure_front_right",
+                "Graphics_mfd_tyre_pressure_rear_left",
+                "Graphics_mfd_tyre_pressure_rear_right",
+                "Physics_steer_angle",
+                "Physics_gear",
+                "Physics_tc",
+                "Physics_abs",
+                "Graphics_tc_cut_level",
+                "Physics_brake_bias",
+                "Physics_road_temp",
+                "Physics_air_temp",
+                "Graphics_wind_speed",
+                "Physics_speed_kmh"
+                ]
+
+df = df[feature_list]
+
+df.to_csv('output_clean_split.csv',index=False)
 
